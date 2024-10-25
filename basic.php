06fbff840020d8083d935e3880cfe7d0188bcb40
saif ur rehman <saif1312687@gmail.com>
2024-10-25 06:54:12 +0500

    php

---
+++ basic.php
@@ -0,0 +1,30 @@
+<?php
+
+//  what is echo 
+//  echo is a construct in language and disply browser in output 
+//  use of echo
+//  echo insensetive 
+//  multiple echo in single line
+//  line break in echo
+
+
+
+echo "hello php <br>";   //use br tag
+
+// ECHO "my name is saif";
+
+// ECHO "me first year it student";
+
+// ecHO "hello php";
+
+// echo use a funtion 
+// echo ("hello php");
+
+
+
+
+
+
+
+
+?>
---
+++ variable.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/*$e ="saif";
+
+echo $e;
+
+$text ="php";
+
+echo "i love. $text" . "!";*/
+
+
+
+
+
+
+//          contant variable 
+
+
+
+// define(variablename,value,case-in-sensitive)
+//cannot be use $ sign
+//constant variable are global variable
+
+// define("x",50);
+
+// echo x;
+
+
+
+
+
+define("y",50,true);
+
+echo y;
+
+
+
+
+
+
+
+?>
